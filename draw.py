00b90222836c3bc5598484ecdc6935a53815b226
mmoss <mmoss020@student.wethinkcode.co.za>
2021-09-23 11:42:39 +0200

    Circle Fixed

--- draw.py
+++ draw.py
@@ -4,7 +4,7 @@ import math
 def get_shape():
     valid_shapes = ["pyramid","square","triangle","circle","rhombus", "octagon"]
     while True:
-        shape = input("Shape?: ").lower()
+        shape = input("Shape?: ").strip().lower()
         if shape in valid_shapes:
             break
         #print("Error: invalid shape, please enter one from the list:")
@@ -15,7 +15,7 @@ def get_shape():
 # TODO: Step 1 - get height (it must be int!)
 def get_height():
     while True:
-        height = input("Height?: ")
+        height = input("Height?: ").strip()
         if height.isnumeric():
             height = int(height)  #checks if its a number and then makes it a number, if its not a number, its assigned -1 which will make the boolean below false.
         else:
@@ -30,7 +30,13 @@ def get_height():
     return height
 
 def outlining(stars, outline, rows, height):
-    return stars if ((outline == False) | (rows == height-1)) else (stars[:1] + stars[1:len(stars)-1].replace("*", " ") + stars[len(stars)-1] if rows > 0 else stars)
+    #This takes a row of stars and hollows it out if outline is true.
+    #Code broken up:
+    #if (not outline) or (rows == height) or (rows == 0):
+        #return stars
+    #else:
+        #return stars[:1] + stars[1:len(stars)-1].replace("*", " ") + stars[len(stars)-1]
+    return stars if ((outline == False) | (rows == height-1) | (rows == 0)) else (stars[:1] + stars[1:len(stars)-1].replace("*", " ") + stars[len(stars)-1])
 
 # TODO: Step 2
 def draw_pyramid(height, outline):
@@ -47,21 +53,31 @@ def draw_square(height, outline):
 
 # TODO: Step 4
 def draw_triangle(height, outline):
-    
+
     for rows in range (height): 
         print(outlining((rows+1)*"*", outline, rows, height))
 
+# TODO: Step 6
 def draw_circle(height, outline):
-    for rows in range (height+1): 
-        stars = (round(math.sqrt((height / 2)**2 - (rows-height / 2)**2 ) + height / 2))*"*"
-        print(((height - len(stars)))*" " + outlining(stars * 2, outline, rows , height+1))
-        
+    if height == 1:
+        print("*")
+    elif height == 2:
+        print("**\n**")
+    else:
+        #below code only works for circles of height >= 3
+        height -= 1 #to get the circle to work with my outlining tool, I make the for loop be one size bigger, and to reduce it again, I make height 1 less
+        for rows in range (height+1): 
+            stars = (round(math.sqrt((height / 2)**2 - (rows-height / 2)**2 ) + height / 2))*"*"
+            print(((height - len(stars)))*" " + outlining(stars * 2 , outline, rows , height+1))
+
+# TODO: Step 6       
 def draw_rhombus(height, outline):
     for rows in range(height): 
         print((rows)*" " + outlining((height)*"*", outline, rows, height))
-
+        
+# TODO: Step 6
 def draw_octagon(height, outline):
-    for rows in range ((height//3)): #Top
+    for rows in range (height//3): #Top
         print((height//3 - rows)*" " + outlining((2 * rows + height - 2*(height//3))*"*", outline, rows, height))
     for rows in range (height - 2*(height//3)): #Middle
         print(outlining((height)*"*", outline, rows + height//3, height))
@@ -76,15 +92,15 @@ def draw(shape, height, outline):
     elif shape == "circle": draw_circle(height, outline)
     elif shape == "rhombus": draw_rhombus(height, outline)
     elif shape == "octagon": draw_octagon(height, outline)
-    #elif shape == "custom": draw_shape(height, outline) #I gave up on this feature
+    
 
 # TODO: Step 5 - get input from user to draw outline or solid
 def get_outline():
-    outline = ""
-    while not outline.lower() in ["y", "n"]:
-        outline = input("Outline only? (y/N):")
+    outline = "YEET"
+    while not outline in ["y", "n" , ""]:
+        outline = input("Outline only? (y/N):").strip().lower()
 
-    if outline.lower() == "y":
+    if outline == "y":
         return True
     return False
         
@@ -95,107 +111,3 @@ if __name__ == "__main__":
     print(shape_param)
     print(height_param)
     draw(shape_param, height_param, outline_param)
-
-''' Dead Code :/
-def get_length(x1,y1,x2,y2):
-    print(str(x1) + "," + str(x2) + "  " + str(y1) + "," + str(y2))
-    return math.sqrt((x1-x2)**2 +(y1-y2)**2)
-   
-def create_empty_grid(height):
-    grid = []
-    for n in range(height):
-        grid.append(height*[" "])
-    return grid
-
-def plot_grid(grid, x, y):
-    grid[y][x] = "#"
-    return grid
-
-def count_non_stars(grid):
-    count = 0
-    for y in grid:
-        for x in y: 
-            if x not in [" ", "*"]:
-                count += 1
-    return count
-        
-def connect_dots(grid):
-    first_co_ords = [-1, -1]
-    
-    while count_non_stars(grid) > 0:
-
-        second_co_ords = [-1, -1]
-        for y in range(len(grid)):
-            for x in range(len(grid)):
-
-                if (grid[y][x] == "#") & (first_co_ords == [-1, -1]):
-                    first_co_ords =  [x, y]
-                    grid[y][x] = "$"
-                    continue
-                
-                if (grid[y][x] in ["#", "$"]):
-                    if (second_co_ords == [-1, -1]):
-                        second_co_ords = [x, y] if not ([x, y] == first_co_ords) else second_co_ords
-                    elif (get_length(first_co_ords[0], first_co_ords[1], second_co_ords[0], second_co_ords[1]) > get_length(first_co_ords[0], first_co_ords[1], x,y)): 
-                        second_co_ords = [x, y]
-                    elif (get_length(first_co_ords[0], first_co_ords[1], second_co_ords[0], second_co_ords[1]) == get_length(first_co_ords[0], first_co_ords[1], x,y)):
-                        second_co_ords = [x, y] if ((grid[y][x] == "#") & (grid[second_co_ords[1]][second_co_ords[0]] == "$")) else second_co_ords
-                    continue
-
-        grid[second_co_ords[1]][second_co_ords[0]] = "$"
-
-        delta_x = first_co_ords[0]-first_co_ords[0]
-        delta_y = first_co_ords[1]-first_co_ords[1]
-            
-        if delta_x == 0:
-            for i in range(first_co_ords[1]+1, second_co_ords[1]):
-                grid[i][first_co_ords[0]] = "*"
-        elif delta_y == 0:
-            for i in range(first_co_ords[0]+1, second_co_ords[0]):
-                grid[first_co_ords[1]][i] = "*"
-        else:
-            gradient = delta_y/delta_x
-            constant = first_co_ords[1] - gradient*first_co_ords[0]
-            for i in range(first_co_ords[0]+1, second_co_ords[0]):
-                grid[round(gradient*i+constant)][i] = "*"
-
-        if grid[first_co_ords[1]][first_co_ords[0]] == "$":
-            grid[first_co_ords[1]][first_co_ords[0]] = "*"
-        else:
-            first_co_ords = second_co_ords
-
-
-    for y in grid:
-        for x in y: 
-            print(x, end = "")
-        print("")
-    return(grid)
-
-def draw_shape(height, outline):
-    grid = create_empty_grid(height)
-    
-    stdin = sys.stdin
-    print("Co-ordinates need to be between: 0 and " + str(len(grid)-1))
-    print("Enter co-ordinates ([x] [y]) (type 'done' to finish) : ", end = "")
-    for line in stdin:
-        
-        input = line.strip()
-        if "done" == input:
-            break
-        else:
-            try:
-                x = int(input[:input.find(" ")])
-                y = int(input[input.find(" ")+1:])
-                print("[" + str(x) + " ; " + str(y) + "]")
-            except:
-                print("Error: invalid input! Use integers only")
-            if (0 <= x <= height - 1) & (0 <= y <= height - 1)  :
-                grid = plot_grid(grid, x, y)
-                continue
-            else:
-                print("Error: Numbers not in range")
-                print("Co-ordinates need to be between: 0 and " + str(len(grid)-1))
-        
-        print("Enter co-ordinates ([x] [y]) : ", end = "")
-    connect_dots(grid)
-'''
